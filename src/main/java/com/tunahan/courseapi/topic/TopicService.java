23be58d39a1a8593f76e77db0fe8f6b57c6898cf
tunahan <bstunahan@gmail.com>
2023-02-17 21:35:42 +0100

    topics mit datenbankverbindung (HSQLDB)

--- src/main/java/com/tunahan/courseapi/topic/TopicService.java
+++ src/main/java/com/tunahan/courseapi/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.tunahan.courseapi.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,34 +10,40 @@ import java.util.List;
 // Service ist ein Singleton welches im Controller injected wird
 @Service
 public class TopicService {
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Topic("spring","Spring Framework","Spring Framework Description"),
-            new Topic("java","Core Java","Core Java Description"),
-            new Topic("js","Javascript","Javascript Description")
-    ));
+
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getAllTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        //return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findById(id).get();
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic) {
+        /*
         for (int i = 0; i < topics.size(); i++) {
             Topic t = topics.get(i);
 
             if(t.getId().equals(id))
                 topics.set(i,topic);
         }
+         */
+        topicRepository.save(topic);
+
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equals(id));
+        // topics.removeIf(t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
